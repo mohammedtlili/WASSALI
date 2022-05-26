7cc34e6f7257f968d8f4f6e02b24af526d1e48e0
Mohamed TLILI <48346056+mohammedtlili@users.noreply.github.com>
2022-05-26 23:24:24 +0200

    commit 8

--- Tarification/Nbre_Klm.py
+++ Tarification/Nbre_Klm.py
@@ -1,12 +1,14 @@
 from typing import Union
-
-from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from enum import Enum
+from fastapi import FastAPI, Body
 
-
-db = []
+# from pydantic import Optional
+from typing import Optional
 
 app = FastAPI()
 
@@ -16,69 +18,129 @@ class Marchandise(BaseModel):
     nbre_objet: int
     label: str
 
-@app.get('/detection')
-def get_detections():
-    return db
-@app.post("/detection")
+
+store_Marchandise = []
+
+
+@app.post("/marchandise/")
 def add_marchandise(marchandise: Marchandise):
-    db.append(marchandise.dict)
-    return db[-1]
+    store_Marchandise.append(marchandise)
+    return marchandise
 
-@app.get("/detection/{id_marchandise}")
+
+@app.get("/marchandise/", response_model=list[Marchandise])
+def read_all_marchandise():
+    return store_Marchandise
+
+
+@app.get("/marchandise/{id_marchandise}")
 def read_marchandise(id_marchandise: int):
-    marchandise = id_marchandise-1
-    return db[marchandise]
-
-@app.put("/marchandises/{id_marchandise}")
-def update_marchandise(id_marchandise: int, marchandise: Marchandise):
-    db[id] = marchandise
-    return {"message": "operation bien determinée"}
+    try:
+        return store_Marchandise[id_marchandise]
+    except:
+        raise HTTPException(status_code=404, detail="Marchandise n'existe pas")
 
 
-
-class categorie_vihecule(BaseModel):
-    id_categorie_vihecule: int
-
-class categorie(Enum):
-    utilitaire = 1
-    camionnette = 2
-    fourgon = 3
-    porteur = 4
+@app.put("/marchandise/{id_marchandise}")
+def update_marchandise(id_marchandise: int, new_marchandise: Marchandise):
+    try:
+        store_Marchandise[id_marchandise] = new_marchandise
+        return store_Marchandise[id_marchandise]
+    except:
+        raise HTTPException(status_code=404, detail="Marchandise n'existe pas")
 
 
-@app.get("/categorie_vihecule")
-def read_categorie_vihecule(id_categorie_vihecule: int, categorie: Enum):
-    return {"id_categorie_vihecule": id_categorie_vihecule, "Categorie": categorie}
-
-@app.post("/categorie_vihecule}")
-def input_categorie_vihecule(id_categorie_vihecule: int, categorie: Enum):
-    return {"id_categorie_vihecule": id_categorie_vihecule, "Categorie": categorie}
-
-@app.put("/categorie_vihecule")
-def update_marchandise(id_categorie_vihecule: int, categorie: Enum):
-    return {"id_categorie_vihecule": id_categorie_vihecule, "Categorie": categorie}
+class Categorie_vihecule(str, Enum):
+    utilitaire = "Utilitaire"
+    camionnette = "Camionnette"
+    fourgon = "Fourgon"
+    porteur = "Porteur"
 
 
-class transport(BaseModel):
+@app.post("/Vihecule/{vihecule}/")
+def add_Vihecule(vihecule: Categorie_vihecule):
+    return {"Hello": vihecule.value}
+
+
+@app.get("/Vihecule/{vihecule}/")
+def read_all_categorie_vihecule(vihecule: Categorie_vihecule):
+    if (vihecule == Categorie_vihecule.utilitaire):
+        return {"message": "votre vihecule est utilitaire"}
+    elif (vihecule == Categorie_vihecule.camionnette):
+        return {"message": "votre vihecule est camionnette"}
+    elif (vihecule == Categorie_vihecule.fourgon):
+        return {"message": "votre vihecule est fourgon"}
+    return {"message": "votre vihecule est porteur"}
+
+
+"""""
+store_vihecule = []
+
+@app.post("/vihecule/")
+def add_categorie_vihecule(categorie_vihecule: Categorie_vihecule):
+    store_vihecule.append(categorie_vihecule)
+    return categorie_vihecule
+
+@app.get("/vihecule/", response_model=list[Categorie_vihecule])
+def read_all_categorie_vihecule():
+    return store_vihecule
+
+@app.get("/vihecule/{id_vihecule}")
+def read_categorie_vihecule(id_vihecule: int):
+    try:
+        return store_vihecule[id_vihecule]
+    except:
+        raise HTTPException(status_code=404, detail="Categorie vihecule n'existe pas")
+
+
+
+@app.put("/vihecule/{id_vihecule}")
+def update_ategorie_vihecule(id_vihecule: int, new_vihecule: Categorie_vihecule):
+    try:
+        store_Marchandise[id_vihecule] = new_vihecule
+        return store_vihecule[id_vihecule]
+    except:
+        raise HTTPException(status_code=404, detail="Categorie vihecule n'existe pas")
+"""
+
+
+class Transport(BaseModel):
     id_transport: int
     Nombre_km: float
     temps_service: int
     prix: float
 
-@app.get("/transport}")
-def read_transport(id_transport: int, Nombre_km: float, temps_service: int, prix: float):
-    prix = Nombre_km *2
-    return {"id_transport": id_transport, "Le prix ": prix}
 
-@app.put("/transport}")
-def update_transport(id_transport: int, Nombre_km: float, temps_service: int):
-    return {"id_transport": id_transport, "Nombre de Klm": Nombre_km, "Le temps de service ": temps_service}
-
-@app.post("/transport}")
-def output_transport(id_transport: int, Nombre_km: float, temps_service: int):
-    return {"id_transport": id_transport, "Nombre de Klm": Nombre_km, "Le temps de service ": temps_service}
+store_Transport = []
 
 
+@app.post("/Transport/")
+def add_transport(transport: Transport):
+    store_Transport.append(transport)
+
+    return transport
+
+
+@app.get("/Transport/", response_model=list[Transport])
+def read_all_transport():
+    return (store_Transport)
+
+
+@app.get("/Transport/{id_transport}")
+def read_transport(id_transport: int):
+    try:
+        return store_Transport[id_transport]
+    except:
+        raise HTTPException(status_code=404, detail="Transport n'existe pas")
+
+
+@app.put("/Transport/{id_transport}")
+def update_transport(id_transport: int, new_transport: Transport):
+    try:
+        store_Transport[id_transport] = new_transport
+        return Transport[id_transport]
+    except:
+        raise HTTPException(status_code=404, detail="Transport n'existe pas")
 
 
 @app.get("/")
@@ -86,5 +148,5 @@ def read_root():
     return {"application de tarification": "phase de test "}
 
 
-
-
+if __name__ == "__main__":
+    uvicorn.run("Nbre_Klm.py: app")
